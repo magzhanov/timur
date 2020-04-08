16901494f13b52e94335ef8a839ea7770f8208fd
Kislichenko Evgeny <kislichenkojeka@mail.ru>
2020-04-08 18:40:16 +0300

    FIx terms

--- Terms.py
+++ Terms.py
@@ -44,17 +44,33 @@ class FiscalTermsSpider(Spider):
 
     # Finds Terms tables on the other sites and checks them:
     def task_table(self, grab, task):
+        root = urlparse(task.url).hostname
+
         try:
             term_url = grab.doc.select(
                 '//script[contains(text(),"open_terms()")]'
             ).text()
             term_url = term_url.split("window.open(")[1].split('"')[1]
+            yield Task(
+                "term",
+                url="http://" + root + term_url,
+                lasturl=task.url,
+                year=task.year,
+                aucType=task.aucType,
+            )
         except:
             # Dealing with common error (Terms' page formating)
             try:
                 term_url = grab.doc.select(
                     '//a[contains(text(), "Terms")]/@href'
                 ).text()
+                yield Task(
+                    "term",
+                    url="http://" + root + term_url,
+                    lasturl=task.url,
+                    year=task.year,
+                    aucType=task.aucType,
+                )
             except Exception as e:
                 try:
                     # Dealing with common error (there is no direct link on Terms) -
@@ -79,25 +95,8 @@ class FiscalTermsSpider(Spider):
                         task.aucType,
                     )
 
-        # Just a useful part of auction's url:
-        root = urlparse(task.url).hostname
-
-        try:
-            # use task_term function for this url:
-            yield Task(
-                "term",
-                url="http://" + root + term_url,
-                lasturl=task.url,
-                year=task.year,
-                aucType=task.aucType,
-            )
-        except Exception as e:
-            self.addError(
-                task.year,
-                getattr(e, "message", str(e)) + " INFO",
-                task.url,
-                task.aucType,
-            )
+    def task_selection_fallback(self, task):
+        self.addError(task.year, "Connection Faild", task.url, task.aucType)
 
     # This function is for "indirect terms":
     def task_selection(self, grab, task):
@@ -121,6 +120,9 @@ class FiscalTermsSpider(Spider):
                 task.url,
                 task.aucType,
             )
+
+    def task_term_fallback(self, task):
+        self.addError(task.year, "Connection Faild", task.url, task.aucType)
 
     # Final part: takes the data and writes it down:
     def task_term(self, grab, task):
